029234fc1a255f3392e3c35c2455d86b29e2a635
RNanclares <raulnanclares@gmail.com>
2018-12-06 11:10:43 -0600

    Añadido script para postgis

---
+++ postgis_install_script.sh
@@ -0,0 +1,7 @@
+sudo apt-get install curl ca-certificates
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+sudo apt-get update
+sudo apt-get install postgresql-11 postgresql-11-postgis-2.5 postgresql-11-postgis-2.5-scripts postgresql-11-pgrouting pgadmin4
+
--- qgis_install_script.sh
+++ qgis_install_script.sh
@@ -1,4 +1,7 @@
+sudo sh -c 'echo "deb http://qgis.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list'
+sudo sh -c 'echo "deb-src http://qgis.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list'
 wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import 
 gpg --fingerprint CAEB3DC3BDF7FB45
 gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
+sudo apt update
 sudo apt-get install qgis python-qgis qgis-plugin-grass
