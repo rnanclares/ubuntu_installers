50f313e81bd3da43657b11ecce9f505e9679ab0e
rnanclares <raulnanclares@gmail.com>
2018-12-13 22:45:40 -0600

    corregidos errores en el script de qgis

--- qgis_install_script.sh
+++ qgis_install_script.sh
@@ -1,5 +1,5 @@
-sudo sh -c 'echo "deb http://qgis.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list'
-sudo sh -c 'echo "deb-src http://qgis.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list'
+sudo sh -c 'echo "deb http://qgis.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb-src http://qgis.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list'
 wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import 
 gpg --fingerprint CAEB3DC3BDF7FB45
 gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
