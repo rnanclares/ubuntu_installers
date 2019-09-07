a29219042f3949f39a198dfc45c4a7e2ba65f5da
rnanclares <RNanclares@users.noreply.github.com>
2019-09-07 15:11:42 -0500

    Update qgis_install_script.sh
    Updated for 2019 gpg key.

--- qgis_install_script.sh
+++ qgis_install_script.sh
@@ -1,7 +1,7 @@
 sudo sh -c 'echo "deb http://qgis.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb-src http://qgis.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list'
-wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import 
-gpg --fingerprint CAEB3DC3BDF7FB45
-gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
+wget -O - https://qgis.org/downloads/qgis-2019.gpg.key | gpg --import
+gpg --fingerprint 51F523511C7028C3
+gpg --export --armor 51F523511C7028C3 | sudo apt-key add -
 sudo apt update
 sudo apt-get install qgis python-qgis qgis-plugin-grass
