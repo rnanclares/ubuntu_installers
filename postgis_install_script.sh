282d13c6325bfa668585d6a8db1b25f67507242b
Raul Nanclares <RNanclares@users.noreply.github.com>
2021-04-04 12:31:31 -0500

    Update postgis_install_script.sh

--- postgis_install_script.sh
+++ postgis_install_script.sh
@@ -3,5 +3,5 @@ curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 sudo apt-get update
-sudo apt-get install postgresql-11 postgresql-11-postgis-2.5 postgresql-11-postgis-2.5-scripts postgresql-11-pgrouting pgadmin4
+sudo apt-get install postgresql-13 postgresql-13-postgis-3.1 postgresql-13-postgis-3.1-scripts postgresql-13-pgrouting pgadmin4
 
